ae11561e1b1b7ca3a2dca9aa4f68dbcd21afba15
Cameron Johnston <cameron@needle.com>
2014-01-02 00:09:01 -0700

    bump to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@needle.com'
 license          'Apache 2.0'
 description      'Installs/Configures leeroy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends          'python'
 depends          'gunicorn'
